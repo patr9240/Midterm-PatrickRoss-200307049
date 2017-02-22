b7a9e3d15a446fd45cb5e54d088621389b7077a6
Patrick Ross <Patrick Ross>
2017-02-22 10:21:30 -0500

    Can add new list items, delete list items.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -19,14 +19,12 @@ export class HomePage {
     this.shoppingListItems = af.database.list('/shoppingList');
   }
   
-   /*updateItem
-    This method activates a popup for the user
-    to edit the details of a to-do list item*/
-  minusItem(itemId, itemCount){
+   /*minusItemCount
+    This method reduces the selected items count by 1 if it is greater than 0*/
+  minusItemCount(itemId, itemCount){
       if(itemCount >= 1){
-        //itemCount = itemCount - 1;
       this.shoppingListItems.update(itemId, {
-        itemCount: itemCount -1
+        itemCount: itemCount - 1
       });
     }
     if(itemCount <= 0){
@@ -35,10 +33,54 @@ export class HomePage {
       });
     }
   }
-
-  addItem(itemId, itemCount){
+  /*addItemCount
+    This method icreases the selected items count by 1*/
+  addItemCount(itemId, itemCount){
       this.shoppingListItems.update(itemId, {
         itemCount: itemCount + 1
       });
+  }
+
+   /*addItem
+    This method activates a popup for the user 
+    to create a new item in the shopping list*/
+  addItem(){    
+    let prompt = this.alertCtrl.create({
+    title: 'Shopping List Item',
+    message: "Add a new item.",
+    inputs: [
+      {
+        name: 'item',
+        placeholder: 'Item'
+      },
+      {
+        name: 'itemCount',
+        placeholder: `0`
+      }
+    ],
+    buttons: [
+       {
+         text: 'Cancel',
+         handler: data => {
+           console.log('Cancel clicked');
+         }
+       },
+       {
+         text: 'Save',
+         handler: data => {
+           this.shoppingListItems.push({
+             item: data.item,
+             itemCount: parseInt(data.itemCount)
+           });
+         }
+       }
+     ]
+   });
+   prompt.present();
   }  
+  /*removeItem
+    This method removes the selected item from the list*/
+  removeItem(itemId: string){
+    this.shoppingListItems.remove(itemId);
+  }
 }
